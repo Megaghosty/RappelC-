6e8d2676cb6faa03db8e28642975bdc6a2eaf96a
Megaghosty <153129021+Megaghosty@users.noreply.github.com>
2024-09-05 12:22:36 +0200

    Added header and some functions

---
+++ Mission.cpp
@@ -0,0 +1,99 @@
+<<<<<<< HEAD
+#include "mission.h"
+#include <iostream>
+using namespace std;
+
+Mission::Mission() : nbPerssone(0), descriptionMission(""), nbHeure(0) {}
+
+Mission::~Mission() {}
+
+
+Mission::Mission(const Mission& miss)
+    : nbPerssone(miss.nbPerssone), descriptionMission(miss.descriptionMission), nbHeure(miss.nbHeure) {}
+
+
+Mission& Mission::operator=(const Mission& miss) {
+    if (this != &miss) {
+        this->nbPerssone = miss.nbPerssone;
+        this->descriptionMission = miss.descriptionMission;
+        this->nbHeure = miss.nbHeure;
+    }
+    return *this;
+}
+
+
+int Mission::GetNombredepersonne() {
+    return nbPerssone;
+}
+
+void Mission::setNombredepersonne(int nb) {
+    this->nbPerssone = nb;
+}
+
+
+std::string Mission::getDescription() {
+    return descriptionMission;
+}
+
+void Mission::setDescription(const string& desc) {
+    this->descriptionMission = desc;
+}
+
+
+int Mission::GetnbHeure() {
+    return nbHeure;
+}
+
+void Mission::setNbHeures(int nbHeures) {
+    this->nbHeure = nbHeures;
+}
+=======
+#include "mission.h"
+#include <iostream>
+
+
+Mission::Mission() : nbPerssone(0), descriptionMission(""), nbHeure(0) {}
+
+Mission::~Mission() {}
+
+
+Mission::Mission(const Mission& miss)
+    : nbPerssone(miss.nbPerssone), descriptionMission(miss.descriptionMission), nbHeure(miss.nbHeure) {}
+
+
+Mission& Mission::operator=(const Mission& miss) {
+    if (this != &miss) {
+        this->nbPerssone = miss.nbPerssone;
+        this->descriptionMission = miss.descriptionMission;
+        this->nbHeure = miss.nbHeure;
+    }
+    return *this;
+}
+
+
+int Mission::GetNombredepersonne() {
+    return nbPerssone;
+}
+
+void Mission::setNombredepersonne(int nb) {
+    this->nbPerssone = nb;
+}
+
+
+std::string Mission::getDescription() {
+    return descriptionMission;
+}
+
+void Mission::setDescription(const std::string& desc) {
+    this->descriptionMission = desc;
+}
+
+
+int Mission::GetnbHeure() {
+    return nbHeure;
+}
+
+void Mission::setNbHeures(int nbHeures) {
+    this->nbHeure = nbHeures;
+}
+>>>>>>> e29a1c879b159941a2dd50674bb74750ffdd7dfb
---
+++ main.cpp
@@ -0,0 +1,47 @@
+<<<<<<< HEAD
+#include <iostream>
+#include "entreprise.h"
+#include "mission.h"
+
+int main() {
+    
+    Entreprise e1("TechCorp", "Paris");
+
+    
+    Mission mission1;
+    mission1.setDescription("Develop new software platform");
+    mission1.setNombredepersonne(20);
+    mission1.setNbHeures(400);
+
+    
+    e1.assignMission(mission1);
+
+    
+    e1.printMissionDetails();
+
+    return 0;
+}
+=======
+#include <iostream>
+#include "entreprise.h"
+#include "mission.h"
+
+int main() {
+    
+    Entreprise e1("TechCorp", "Paris");
+
+    
+    Mission mission1;
+    mission1.setDescription("Develop new software platform");
+    mission1.setNombredepersonne(20);
+    mission1.setNbHeures(400);
+
+    
+    e1.assignMission(mission1);
+
+    
+    e1.printMissionDetails();
+
+    return 0;
+}
+>>>>>>> e29a1c879b159941a2dd50674bb74750ffdd7dfb
